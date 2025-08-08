bb4618a22d392f8e036cc228793439f5941246a1
JayasriGirijala <girijalajayasri@gmail.com>
2025-08-09 00:14:40 +0530

    Authorization21

---
+++ src/main/java/com/springboot21/GroceryApiApplication.java
@@ -0,0 +1,12 @@
+package com.springboot21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryApiApplication.class, args);
+    }
+}
+//http://localhost:8080/grocery/public          --get
---
+++ src/test/java/com/springboot21/grocery_api/GroceryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot21.grocery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
