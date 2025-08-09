4a2c10fe82e48811d07d6c12cde8074604256f4c
Girijala Veera Venkata Jaya sri <86398853+JayasriGirijala@users.noreply.github.com>
2025-08-09 10:19:26 +0530

    Update GroceryApplication.java

--- src/main/java/com/groceryapp/GroceryApplication.java
+++ src/main/java/com/groceryapp/GroceryApplication.java
@@ -10,3 +10,31 @@ public class GroceryApplication {
         SpringApplication.run(GroceryApplication.class, args);
     }
 }
+
+//http://localhost:8080/api/groceries
+/*[
+{
+"id": 1,
+"name": "Apples",
+"quantity": 10,
+"price": 2.5
+},
+{
+"id": 2,
+"name": "Bananas",
+"quantity": 12,
+"price": 1.5
+},
+{
+"id": 3,
+"name": "Carrots",
+"quantity": 8,
+"price": 1.2
+},
+{
+"id": 4,
+"name": "Tomatoes",
+"quantity": 15,
+"price": 2.0
+}
+]*/
